7ee77c038d0bca51cc30ac56a75f1acb12b44154
georges.dick@gmail.com <georges.dick@gmail.com@4b2a07b1-c1eb-6577-e556-604b1e082164>
2011-02-01 07:29:08 +0000

    Clean spam folder too...

--- trunk/config/config.inc.php
+++ trunk/config/config.inc.php
@@ -4,5 +4,5 @@
 $rcmail_config['foldermaintenance_max_days'] = 120;
 $rcmail_config['foldermaintenance_red_ratio'] = 4;
 
-$rcmail_config['foldermaintenance_startup_folders'] = "Trash,Sent";
+$rcmail_config['foldermaintenance_startup_folders'] = "Trash,Sent,spam-mail";
 ?>
