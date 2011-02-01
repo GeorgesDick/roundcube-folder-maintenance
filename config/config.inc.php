3b1c312e839790cf5f05950f88a96f705a0eafa3
georges.dick@gmail.com <georges.dick@gmail.com@4b2a07b1-c1eb-6577-e556-604b1e082164>
2011-02-01 15:28:34 +0000

    Deuz en plus

--- config/config.inc.php
+++ config/config.inc.php
@@ -4,6 +4,6 @@
 $rcmail_config['foldermaintenance_max_days'] = 120;
 $rcmail_config['foldermaintenance_red_ratio'] = 4;
 
-// Folder list
+// Folder list (Trash + Sent messages)
 $rcmail_config['foldermaintenance_startup_folders'] = "Trash,Sent,mail-trash";
 ?>
