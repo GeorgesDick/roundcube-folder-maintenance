ab14c66cd63a6b5895a25072037606b484905bf4
georges.dick@gmail.com <georges.dick@gmail.com@4b2a07b1-c1eb-6577-e556-604b1e082164>
2011-02-01 16:58:31 +0000

    Améliorations

--- trunk/config/config.inc.php
+++ trunk/config/config.inc.php
@@ -4,6 +4,6 @@
 $rcmail_config['foldermaintenance_max_days'] = 120;
 $rcmail_config['foldermaintenance_red_ratio'] = 4;
 
-// Folder list (Trash + Sent messages + Alternate trash)
-$rcmail_config['foldermaintenance_startup_folders'] = "Trash,Sent,mail-trash";
+// Folder list (Trash + sent messages + spam folder + alternate trash)
+$rcmail_config['foldermaintenance_startup_folders'] = "Trash,Sent,spam-mail,mail-trash";
 ?>
